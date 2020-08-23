ad88e073782a8cbe702bce152f71f980eb0d95ed
Pachauri <prajjwalpachauri725@gmail.com>
2020-08-22 22:33:22 -0700

    add fields to Video.java

---
+++ src/definition/Video.java
@@ -0,0 +1,14 @@
+package definition;
+
+public class Video {
+
+    // create definition classes
+    // Rules:
+    // 1. Always set the visibility modifier of fields to private unless otherwise specified.
+    // 2. Always set the aess modifier of fields to non static unless otherwise specified.
+    // fields
+    private String videoName;
+    private int rating;
+    private boolean checkOut;
+
+}
