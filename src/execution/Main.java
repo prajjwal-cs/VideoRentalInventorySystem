72d215488609a8d89434ba592be20486881e9210
Pachauri <prajjwalpachauri725@gmail.com>
2020-08-23 22:38:56 -0700

    execute the video class in Main.java class

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,7 +1,20 @@
 package execution;
 
+import definition.Video;
+
 public class Main {
     public static void main(String[] args) {
-
+        Video video = new Video("Bahubali", 5, false);
+        System.out.println(
+                "Video Name: " + video.getVideoName() + ", " +
+                        "Video rating: " + video.getRating() + ", " +
+                        "Is Video Available: " + video.isCheckOut() + "."
+        );
+        Video video1 = new Video();
+        System.out.println(
+                "Video Name: " + video1.getVideoName() + ", " +
+                        "Video rating: " + video1.getRating() + ", " +
+                        "Is Video Available: " + video1.isCheckOut() + "."
+        );
     }
 }
